e630f0faac1ddcdd34af9532467b7d899776f0bb
Gianfranco De Paula <baciliogianfranco@gmail.com>
2024-03-27 04:49:01 -0400

    [ADD] DB Config

--- management/settings.py
+++ management/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local Apps
+    'management',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -79,9 +83,13 @@ WSGI_APPLICATION = 'management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('DB_NAME'),
+       'USER': os.getenv('DB_USER'),
+       'PASSWORD': os.getenv('DB_PASSWORD'),
+       'HOST': os.getenv('DB_HOST'),
+       'PORT': os.getenv('DB_PORT')
+   }
 }
 
 
